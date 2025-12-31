dceb17117fc1d8db22d914a4b20f14a03fd898b0
Suraj Kumar <suraj.k@avantaifintech.com>
2025-12-31 11:05:13 +0530

    buffer final -> learned alloc, allocunSafe, and from method

--- 2-understanding-buffers/app.js
+++ 2-understanding-buffers/app.js
@@ -1,10 +1,9 @@
 import { Buffer } from "node:buffer";
-/*
+
 const memoryContainer = Buffer.alloc(4); // Allocate 4 bytes of memory
 memoryContainer.writeInt8(-40, 0); // Write the value -40 at the first byte
 console.log(memoryContainer);
 console.log(memoryContainer.readInt8(0)); // Read the value at the first byte
-*/
 
 // Challenge
 /*
@@ -19,5 +18,10 @@ console.log(challengeMemoryContainer.toString("utf-8"));
 const challengeMemoryContainer = Buffer.from([0x48, 0x69, 0x21]);
 console.log(challengeMemoryContainer.toString("utf-8"));
 */
+// Fastest way to create a buffer is using Buffer.from() and Buffer.allocUnsafe()
 const challengeMemoryContainer = Buffer.from("486921", "hex");
 console.log(challengeMemoryContainer.toString("utf-8"));
+console.log(challengeMemoryContainer.byteLength);
+
+const bytesLength = Buffer.byteLength("Suraj", "utf-8");
+console.log(bytesLength);
